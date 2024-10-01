eb5a645b56aa97eb758c7251da0420e9148b3334
Mark Levy <levymark@gmail.com>
2024-10-01 23:13:44 +0300

    feat: render cards, open and close modal with form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import "./Header.css";
 import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/Avatar.png";
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <header className="header">
       <img alt="Logo" src={logo} className="header__logo" />
       <p className="header__date-and-location">DATE, LOCATION</p>
-      <button className="header__add-clothes-btn">+ Add clothes</button>
+      <button
+        onClick={handleAddClick}
+        type="button"
+        className="header__add-clothes-btn"
+      >
+        + Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__user-name">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
