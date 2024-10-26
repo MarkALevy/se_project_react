f905f53fdf90f2bddd7298da925d8ddbeb557b39
Mark Levy <levymark@gmail.com>
2024-10-27 00:15:27 +0300

    update baseUrl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -78,7 +78,10 @@ export const coordinates = {
 };
 
 export const APIkey = "130f0b6a4fadc2899b6d71c8934be8e1";
-export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://www.wtwr2024.serverpit.com/"
+    : "http://localhost:3001";
 export const headers = {
   "Content-Type": "application/json",
 };
